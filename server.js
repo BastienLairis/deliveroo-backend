3c24dffdb783e11d3ed6fdf3b99c710f1ac114cd
bastienlairis <bastien.lairis@gmail.com>
2022-07-26 10:13:36 +0200

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const data = require("./data.json");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   //J'envoie ma data au client
